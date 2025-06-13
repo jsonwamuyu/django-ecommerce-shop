01afff342bcb1e2b064c6884066d35dfb82ab204
jsonwamuyu <jysonmuchiri@gmail.com>
2025-06-13 23:20:01 +0300

    Apply css styles

---
+++ shop/forms.py
@@ -0,0 +1,23 @@
+# shop/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2")
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline',
+                'placeholder': 'Username',
+            }),
+            'password1': forms.PasswordInput(attrs={
+                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline',
+                'placeholder': 'Password',
+            }),
+            'password2': forms.PasswordInput(attrs={
+                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline',
+                'placeholder': 'Confirm Password',
+            }),
+        }
--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from .forms import CustomUserCreationForm
+
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)  # Log the user in immediately after signup
             return redirect('shop:product_list')  # Redirect to product list or homepage
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm(request.POST)
     return render(request, 'shop/register.html', {'form': form})
 
 
